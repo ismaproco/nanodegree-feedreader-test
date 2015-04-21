ef4cebb197969dbedad75de7f355e03b03b122b3
Ismael Jimenez <ijimenezgarzon@gmail.com>
2015-04-21 11:58:01 -0600

    Added changes necessary for the feed selection

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -108,21 +108,28 @@ $(function() {
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
-    // execute the load function for the first element on the allFeed list.
-    it('loadFeed changes the content in the .feed container', function(done) {
-        var innerContentHTML = '';
-        loadFeed(0, function() {
-            // gets the html for the loaded element
-            innerContentHTML = $('.feed').html();
+    describe('New Feed Selection', function() {
+        /* TODO: Write a test that ensures when a new feed is loaded
+         * by the loadFeed function that the content actually changes.
+         * Remember, loadFeed() is asynchronous.
+         */
 
-            // load another feed
-            loadFeed(1, function() {
-                // compare both contents to be different
-                expect($('.feed').html() !== innerContentHTML).toBe(true);
-                // call the done validation
-                done();
+         // execute the load function for the first element on the allFeed list.
+         it('loadFeed changes the content in the .feed container', function(done) {
+            var innerContentHTML = '';
+            loadFeed(0, function() {
+                // gets the html for the loaded element
+                innerContentHTML = $('.feed').html();
+
+                // load another feed
+                loadFeed(1, function() {
+                    // compare both contents to be different
+                    expect($('.feed').html() !== innerContentHTML).toBe(true);
+                    // call the done validation
+                    done();
+                });
             });
-        });
+         });
     });
 
 
